35c83954fa0e109cf94bae62549f34766572f4b7
laste <liruikaiyao@gmail.com>
2015-05-07 16:52:25 +0800

    finish new user tag, add active user file, add new user config file

--- lyf_tag/config/db.py
+++ lyf_tag/config/db.py
@@ -1,4 +1,3 @@
-import pymongo
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
@@ -6,3 +5,4 @@ ICCv1 = client['ICCv1']
 umav3 = client['umav3']
 bda = client['bda']
 test = client['test']
+mapreduce = client['mapreduce']
