0adf93e5770a1188921aa0a02afe7e3d0011db04
laste <liruikaiyao@gmail.com>
2015-05-22 16:37:44 +0800

    modify get city to be a function and save to mongoDB

--- add_tag/city_tag.py
+++ add_tag/city_tag.py
@@ -10,16 +10,26 @@ import urllib2
 import json
 
 lyf = ICCv1['lyf']
+user_info = mapreduce['user_info']
 api_url = u'http://api.map.baidu.com/geocoder/v2/?ak='
 parameter = u'&output=json&coordtype=wgs84ll&location='
 ak = 'SIpMcORCSogM916QMOz5tx7S'
 
-for elem in lyf.find({'Event': 'LOCATION'}):
-    location = elem['Latitude'] + ',' + elem['Longitude']
+
+# 根据lbs数据获取城市信息的函数
+def getcity(one):
+    location = one['Latitude'] + ',' + one['Longitude']
     result = urllib2.urlopen(api_url + ak + parameter + location)
     try:
         address = json.loads(result.read())['result']
         city = address['addressComponent']['city']
+        two = dict()
+        two['FromUserName'] = one['FromUserName']
+        two['city'] = city
+        user_info.insert_one(two)
     except Exception as e:
         print e
-        continue
+
+
+for elem in lyf.find({'Event': 'LOCATION'}):
+    getcity(elem)
