de2d779dc2f393fd6a2cfa9591defee1cefdeb92
laste <liruikaiyao@gmail.com>
2015-06-19 10:18:57 +0800

    add fim to analysis tags and user net

---
+++ sch_tag/kanjia.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+__author__ = 'Carry lee'
+
+from config.db import sch, ICCv1, test
+from collections import defaultdict, Counter, OrderedDict
+import networkx as nx
+import matplotlib.pyplot as plt
+
+desc = u"553db17b4796197c7c58a84f"
+kanjia = sch['kanjia']
+kanjiawu = sch['kanjiawu']
+kanjia_net = OrderedDict()
+identity_list = [elem['code'] for elem in kanjiawu.find().sort('__CREATE_TIME__', 1)]
+
+for elem in identity_list:
+    kanjia_net[elem] = []
+
+for elem in kanjiawu.find().sort('__CREATE_TIME__', 1):
+    every_key = []
+    name = elem['name']
+    cursor = kanjia.find({'memo.name':name})
+    for item in cursor:
+        every_key.append(item['identity_id'])
+    kanjia_net[elem['code']].extend(list(set(every_key)))
+
+# 计算第一层用户
+
+
+first_level = []
+all_user = []
+
+for k, v in kanjia_net.items():
+    if k not in all_user:
+        first_level.append(k)
+        all_user.extend(v)
+    else:
+        all_user.extend(v)
+
+print(len(first_level))
+
+second_level = []
+for elem in first_level:
+    second_level.extend(kanjia_net[elem])
+
+print(len(second_level))
+
+print(len(all_user))
+
+# 生成网图
+G=nx.Graph()
+for k,v in kanjia_net.items():
+    for elem in v:
+        G.add_edge(k,elem)
+
+print(G.number_of_edges())
+print(G.number_of_nodes())
+
